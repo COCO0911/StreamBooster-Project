2cd1118a7f8a9a6920509b9f16bcc28c2e33f135
Yaxuan Li <licody20@gmail.com>
2022-03-05 11:30:19 -0800

    initial commit:  register/ login/ logout

---
+++ src/main/java/cocoli/introduction/service/RegisterService.java
@@ -0,0 +1,21 @@
+package cocoli.introduction.service;
+
+import cocoli.introduction.dao.RegisterDao;
+import cocoli.introduction.entity.response.db.User;
+import cocoli.introduction.util.Util;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.io.IOException;
+
+@Service
+public class RegisterService {
+
+    @Autowired
+    private RegisterDao registerDao;
+
+    public boolean register(User user) throws IOException {
+        user.setPassword(Util.encryptPassword(user.getUserId(), user.getPassword()));
+        return registerDao.register(user);
+    }
+}
+
