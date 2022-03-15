5e3a302e8a83751d8b36f3f1da03efe7b1e91a97
Coco <licody20@gmail.com>
2022-03-15 16:38:57 -0500

    recommendation

---
+++ src/main/java/cocoli/introduction/service/RecommendationException.java
@@ -0,0 +1,7 @@
+package cocoli.introduction.service;
+
+public class RecommendationException extends RuntimeException {
+    public RecommendationException(String errorMessage) {
+        super(errorMessage);
+    }
+}
